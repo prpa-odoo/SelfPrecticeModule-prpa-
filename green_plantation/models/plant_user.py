85fa4332c017e22a777baee968b7fe3d4fdd5617
priyanshi patel(prpa) <prpa@odoo.com>
2023-02-09 18:33:43 +0530

    model view

--- green_plantation/models/plant_user.py
+++ green_plantation/models/plant_user.py
@@ -1,7 +1,14 @@
 from odoo import fields,models
 
 class PlantUser(models.Model):
-    _name = "plant.user"
-    _description = "User Details"
+   _name = "plant.user"
+   _description = "User Details"
 
-    name = fields.Char(required=True)
+   uname = fields.Char(required=True,string="UserName")
+   address=fields.Text(string="Address")
+   phone_no=fields.Integer(string="PhoneNo")
+   email=fields.Char(string="Email")
+   user_role=fields.Selection(
+      string="Select User",
+      selection=[('customer','Customer'),('farmer','Farmar'),('visitor','Visitor'),('caretaker','Caretaker')]
+   )
